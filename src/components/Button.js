3d9fd6ecf76f5e7b8342584e184a787ac4af4ef5
ajosimovska1 <ajosimovska1@gmail.com>
2020-08-20 18:41:51 +0200

    wip

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
+// import PropTypes from 'prop-types';
+// import classNames from 'classnames';
 
-function Button({ onClick, className, outline, children }){
+function Button({ onClick }){
     return(
         <button
         onClick={onClick}
-        className={classNames('button', className, {
-          'button--outline': outline,
-        })}>
-        {children}
+        className="button--cart">
       </button>
     )
 };
 
 
-Button.propTypes = {
-    onClick: PropTypes.func,
-  };
 
 export default Button;
