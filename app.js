4dc3485d87395c0302048c5d94696afc4c20dce4
ajosimovska1 <ajosimovska1@gmail.com>
2020-08-21 12:33:24 +0200

    wip

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-const express =  require('express');
+const express = require('express');
+const cors = require('cors')
 const path = require('path');
-const cors = require('cors');
-const routes = require('./api');
-
+const PORT = process.env.PORT || 3009;
 const app = express();
-
-const PORT = process.env.PORT || 3001
-
-if(!process.env.NODE_ENV || process.env.NODE_ENV !== 'production'){
-    app.use(cors())
-} 
-
-
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-
-app.use('/', express.static(path.join(__dirname, 'build')));
-
-app.use('/api', routes);
-
-app.listen(PORT, () =>{
-    console.log('API started on port' + PORT);
-});
+const router = require('./api/routes');
+const { transferData } = require('./api/utilities');
+const run = async () => {
+    await transferData();
+    if (!process.env.NODE_ENV || process.env.NODE_ENV !== 'production') {
+        app.use(cors())
+    }
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use('/', express.static(path.join(__dirname, 'build')));
+    app.use('/api', router);
+    app.listen(PORT, () => console.log(`Our app listening at ${PORT}`));
+};
+run();
